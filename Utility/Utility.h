1df17da0cdea939f3a96e532d0e24477c248f8e1
Ranga Reddy Nallavelli <thisisranga@gmail.com>
2017-03-16 22:19:57 -0400

    Initial Commit

---
+++ Utility/Utility.h
@@ -0,0 +1,13 @@
+//
+//  Utility.h
+//  Utility
+//
+//  Created by Ranga Reddy Nallavelli on 3/16/17.
+//  Copyright © 2017 Ranga. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Utility : NSObject
+
+@end
